a80954730aa0d503bc265283d3092d1989d682e9
Piyoxz <102013178+Piyoxz@users.noreply.github.com>
2023-12-02 22:51:09 +0700

    Update make-in-memory-store.ts

--- src/Store/make-in-memory-store.ts
+++ src/Store/make-in-memory-store.ts
@@ -1,7 +1,7 @@
 import type KeyedDB from '@adiwajshing/keyed-db'
 import type { Comparable } from '@adiwajshing/keyed-db/lib/Types'
 import type { Logger } from 'pino'
-import type { statSync, existsSync } from 'fs';
+import { statSync, existsSync } from 'fs';
 import { proto } from '../../WAProto'
 import { DEFAULT_CONNECTION_CONFIG } from '../Defaults'
 import type makeMDSocket from '../Socket'
